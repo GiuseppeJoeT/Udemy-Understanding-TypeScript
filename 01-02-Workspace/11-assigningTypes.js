a6aaa8a51f41d5e9a2e2f08e977c0b221106a0f2
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-11-13 11:39:47 +0100

    TypeScript assign Types
    TypeScript assign Types

---
+++ 01-02-Workspace/11-assigningTypes.js
@@ -0,0 +1,10 @@
+// Assign types
+var myRealAge;
+myRealAge = 32;
+myRealAge = '32';
+// The compiler won't complain since I did't assign a type in the first var declaration
+// assigning a type explicitly
+var anotherAge;
+anotherAge = 30;
+// now we get a compiler error
+// anotherAge = '30';
