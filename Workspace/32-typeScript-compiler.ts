d65e7c061c57086a1f16fc70b5c368053cf616d2
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-11-16 12:14:22 +0100

    TtypeScript compiler test
    TtypeScript compiler test with tsconfig.json

--- Workspace/32-typeScript-compiler.ts
+++ Workspace/32-typeScript-compiler.ts
@@ -1,4 +1,16 @@
 let myFirstName: string = 'Joe';
 let myAge:number = 32;
 
-myFirstName = 30;
+// implicit set to ANY type
+let anything;
+anything = 30;
+
+// myFirstName = 30; -> error, the value is a STRING
+
+// Compiler improvements with TypeScript 3.0
+function controlMe(isTrue: boolean, somethingElse: boolean) {
+    let result: number;
+    if (isTrue) {
+        result = 17;
+    }
+}
