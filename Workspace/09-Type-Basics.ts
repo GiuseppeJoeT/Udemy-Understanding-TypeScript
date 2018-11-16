ac6702c9c25b5112418b5448bba6f82e97ea4b82
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-11-16 09:47:48 +0100

    workspace
    workspace

---
+++ Workspace/09-Type-Basics.ts
@@ -0,0 +1,8 @@
+// String
+let myName = 'Joe';
+
+// try to compile it! 
+myName = 32;
+
+// As you can see that type number is not assignable to type string.
+// That cannot be possible in TypeScript with the STATIC STYPES, instead you can do that in JavaScript with Dynamic Types
---
+++ Workspace/10-numbersAndBooleans.ts
@@ -0,0 +1,11 @@
+// Number type
+let integer = 32;
+let floatingPoint = 32.1;
+
+// If it is an integer or a float that is both type a NUMBER, there is no differentiation between that in TypeScript.
+
+// Boolean
+let hasHobbies = true;
+hasHobbies = 0;
+
+// It still gives me an error because one is a number and hasHobbies is a boolean and or it likes to have booleans and not numbers.
