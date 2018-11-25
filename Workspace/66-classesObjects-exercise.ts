353d0ca41c8fd9113ee1a16a5ae2d3da4590de91
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-11-25 19:16:54 +0100

    Create 66-classesObjects-exercise.ts

---
+++ Workspace/66-classesObjects-exercise.ts
@@ -0,0 +1,48 @@
+// Exercise 1 solution
+class Car {
+// superclass
+    
+    acceleration: number;
+
+    constructor(public name: string) {
+        this.acceleration = 0;
+    }
+
+    honk() {
+        console.log("hoooooooooonk!");
+    }
+
+    accelerate(speed: number) {
+        this.acceleration = this.acceleration + speed;
+    }
+}
+// a new Car object
+const car01 = new Car ("Ferrari");
+car01.honk(); // hoooooooooonk!
+console.log("The car accelaration start to: " + car01.acceleration);
+
+let maxSpeed = car01.accelerate(270);
+console.log("The max speed is: " + maxSpeed + " km/h"); 
+
+console.log("The car accelaration start to: " + car01.acceleration);
+
+// Exercise 2 solution
+class BaseObject {
+// superclass
+
+    width: number = 0;
+    length: number = 0;
+}
+
+// INHERITANCE
+class Rectangle extends BaseObject {
+    // base class
+    width: number = 5;
+    length: number = 2;
+    calcSize(width, length) {
+        return this.width * this.length;
+    }
+}
+// a new rectangle object
+const rectangle01 = new (Rectangle);
+console.log(rectangle01.calcSize(5,3));
