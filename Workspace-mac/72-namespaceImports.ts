74c3fe555daf2863aa2e71bf0aca82ee5300f349
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-12-02 18:18:55 +0100

    namespaces and modules

--- Workspace-mac/72-namespaceImports.ts
+++ Workspace-mac/72-namespaceImports.ts
@@ -13,6 +13,9 @@ console.log('Rectangle area: ' + MyMaths.calcuRectangle(10, 5));
 // using the import keyword
 console.log('Circumference area: ' + CircleMath.calcuCircumference(11));
 console.log(Pi);
+console.log('So whilst NAMESPACES here are a great tool for smaller applications you probably want to fall back' 
++ 'to either ES6 modules or modules in general,'
++ 'which are best for working on medium sized and large projects.')
 
 
 
