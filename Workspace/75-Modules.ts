45787b63be1207f13f1fb53616e90e029775d70a
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-12-04 18:29:41 +0100

    Modules

--- Workspace/75-Modules.ts
+++ Workspace/75-Modules.ts
@@ -1,7 +1,7 @@
 // Starting with ECMAScript 2015, JavaScript has a concept of modules. TypeScript shares this concept.
 // TypeScript will look for: .ts , .tsx , .dts file.
-import {PI, calcCircomf} from "./mathModule/circle";
+import {PiCirc, calcCircomf} from "./mathModule/circle";
 
-console.log(PI);
+console.log(PiCirc);
 console.log(calcCircomf(11));
 // console.log();
