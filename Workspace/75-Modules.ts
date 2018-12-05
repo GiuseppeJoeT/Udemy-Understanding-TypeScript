a93b6bb1e7de3ed24e46ff920462ec581d67e7ed
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-12-05 18:10:13 +0100

    Update 75-Modules.ts

--- Workspace/75-Modules.ts
+++ Workspace/75-Modules.ts
@@ -1,7 +1,10 @@
 // Starting with ECMAScript 2015, JavaScript has a concept of modules. TypeScript shares this concept.
 // TypeScript will look for: .ts , .tsx , .dts file.
-import {PiCirc, calcCircomf} from "./mathModule/circle";
+import * as Circle from "./mathModule/circle";
+import { calcRectangleArea } from "./mathModule/rectangle";
+import defaultRec from "./mathModule/dafaultRectangle";
 
-console.log(PiCirc);
-console.log(calcCircomf(11));
-// console.log();
+console.log(Circle.PiCirc);
+console.log(Circle.calcCircomf(11));
+console.log('The rectangle area is: ' + calcRectangleArea(30,50) + ' cm^2');
+console.log('The default rectangle area is: ' + defaultRec(5,3));
