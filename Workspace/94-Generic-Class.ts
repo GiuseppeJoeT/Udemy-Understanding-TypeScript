16ccdc8329beeb31bebf10c4bd95e7ea50a93465
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-12-13 17:20:42 +0100

    Generic class
    multiple type

--- Workspace/94-Generic-Class.ts
+++ Workspace/94-Generic-Class.ts
@@ -1,14 +1,16 @@
 // Generic Class
-class SimpleMath<T extends number | string> {
+
+// more than one Generic type
+class SimpleMath<T extends number | string, U extends number | string> {
     baseValue: T;
-    multiplyValue: T;
+    multiplyValue: U;
     multiply(): number {
         // the plus sign means: force the CAST and convert this value to a NUMBER (in this case) 
         return +this.baseValue * +this.multiplyValue;
     }
 }
 
-const simpleMath = new SimpleMath<string>();
+const simpleMath = new SimpleMath<string, number>();
 simpleMath.baseValue = '10';
-simpleMath.multiplyValue = '20';
+simpleMath.multiplyValue = 20;
 console.log('The moltiplication result is: ' + simpleMath.multiply()); 
