6b6414193d7646724f7d9e6cfaacd7d029683a19
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-12-13 12:29:43 +0100

    enum type

--- Workspace/09-Type-Basics.ts
+++ Workspace/09-Type-Basics.ts
@@ -6,3 +6,10 @@ myName = 32;
 
 // As you can see that type number is not assignable to type string.
 // That cannot be possible in TypeScript with the STATIC STYPES, instead you can do that in JavaScript with Dynamic Types
+
+// Tuple
+let driver: [string, number] = ['Ayrton Senna', 3];
+
+// Enum
+enum PuntoCardinale {Nord, Sud, Ovest, Est};
+const nord: PuntoCardinale = PuntoCardinale.Nord; // 0
