02d6545bcec2d7f01920ebddce1dbe526614a507
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-12-14 12:08:46 +0100

    Update 99-Generic-Constraints.ts

--- Workspace/99-Generic-Constraints.ts
+++ Workspace/99-Generic-Constraints.ts
@@ -1,9 +1,9 @@
-function stampaDettagli<T>(elenco: T[]): void {
-    // ERRORE: Property 'dettagli' does not exist on type 'T'.
-    elenco.forEach(elemento => {
-        // elemento.dettagli(); 
-    });
-}
+// function stampaDettagli<T>(elenco: T[]): void {
+//     // ERRORE: Property 'dettagli' does not exist on type 'T'.
+//     elenco.forEach(elemento => {
+//         // elemento.dettagli(); 
+//     });
+// }
 
 interface HasDetails {
     dettagli(): string;
@@ -29,9 +29,38 @@ class Automobile implements Veicolo {
     dettagli(): string {
         // template strings, ES6
         return `Dettagli auto:
-        marca:${this.marca}
-        modello:${this.modello}
-        anno:${this.anno}
+            marca:${this.marca}
+            modello:${this.modello}
+            anno:${this.anno}
         `;
     }
 }
+
+class Motocicletta implements Veicolo {
+    
+    constructor(public marca: string, public modello: string, public anno?: number) {
+        this.marca = marca;
+        this.modello = modello;
+    }
+
+    dettagli(): string {
+        return `Dettagli moto:
+            marca:${this.marca}
+            modello:${this.modello}
+        `;
+    }
+}
+
+function stampaDettagli<T extends HasDetails>(elenco: T[]): void {
+    elenco.forEach(elemento => {
+        console.log(elemento.dettagli());
+    });
+}
+
+const elencoVeicoli = [
+  new Automobile('Ferrari', '365 GTB/4', 1972),
+  new Automobile('Aston Martin', 'Vantage'),
+  new Motocicletta('Yamaha', 'XSR700')
+];
+
+stampaDettagli<Veicolo>(elencoVeicoli);
