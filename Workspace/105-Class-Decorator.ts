caedbd11b52aefd0e328c904d90e885f0ba8a6a5
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-12-14 13:25:43 +0100

    decorators

--- Workspace/105-Class-Decorator.ts
+++ Workspace/105-Class-Decorator.ts
@@ -1,11 +1,22 @@
 // @Decorators
 //  A Decorator is a special kind of declaration that can be attached to a class declaration, method, accessor, property, or parameter. 
-
 function logged(constructorFn: Function) {
     console.log(constructorFn);
 }
 
 @logged
 class PersonDec {
-    
+    constructor() {
+        console.log('hi!');
+    }
 }
+
+// Factory
+function logging(value: boolean) {
+    return value ? logged : null;
+}
+
+@logging(true)
+class CarFactory {
+
+} 
